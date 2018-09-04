79bd3139478601e120c0ace11877366678c6d7d3
zhuo zheng <zhuozheng@zhuos-MBP.fios-router.home>
2018-09-03 23:36:37 -0400

    update

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {Ingredient} from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredient = new EventEmitter<{name: string, amount: number}>();
+  @Output() deletedIngredient = new EventEmitter<{name: string, amount: number}>();
+  @Output() clearIngredient = new EventEmitter<void>();
+  @Input() choosenIngredient: Ingredient;
   constructor() { }
 
   ngOnInit() {
   }
 
+  addItem() {
+    const newIngredient = new Ingredient(this.nameInputRef.nativeElement.value, parseInt(this.amountInputRef.nativeElement.value, 10));
+    this.ingredient.emit(newIngredient);
+  }
+
+  deleteItem() {
+    if (this.choosenIngredient) { this.deletedIngredient.emit(this.choosenIngredient); }
+  }
+  clear() {
+    this.clearIngredient.emit();
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -12,10 +12,48 @@ export class ShoppingListComponent implements OnInit {
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
   ];
+  choosenIngredient: Ingredient;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onAddIngredient(ingredient: Ingredient) {
+    if (this.ingredients.length !== 0) {
+      var finded = false;
+      for (let i = 0 ; i < this.ingredients.length ; i++ ) {
+        if (this.ingredients[i].name === ingredient.name ) {
+          this.ingredients[i].amount +=  ingredient.amount;
+          finded = true;
+        }
+      }
+      if (!finded) {
+        this.ingredients.push(ingredient);
+      }
+    } else {
+      this.ingredients.push(ingredient);
+    }
+  }
+
+  chose(ingredient: Ingredient) {
+    this.choosenIngredient = ingredient;
+  }
+  delete(ingredient: Ingredient ) {
+    for (let i = 0; i < this.ingredients.length; i++) {
+      if (this.ingredients[i].name === ingredient.name) {
+        this.ingredients.splice(i, 1);
+      }
+    }
+  }
+    getColor(ingredient: Ingredient) {
+      if (ingredient && this.choosenIngredient) {
+        if (ingredient.name === this.choosenIngredient.name) { return '#F5F5F5'; }
+      }
+      return '#FFFFFF';
+    }
+
+    clear() {
+      this.ingredients = [];
+    }
 }
